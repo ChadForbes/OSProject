4026a87ce128319a8963a47f4f9358f38b20ac59
Chad Forbes <ninjagamer32@gmail.com>
2019-03-14 23:34:06 -0400

    Update Conversions.java

--- src/Conversions.java
+++ src/Conversions.java
@@ -1,21 +1,32 @@
+// Author Ryan Wheeler
+import java.lang.Math;
 
 public class Conversions 
 {
+	// method to convert a hex string to a binary int
 	public static int hexToDecimal(String hex) 
 	{
+		//Long i = Long.parseLong(hex, 16);
+
+		//int j = Math.toIntExact(i);
+		//return j;
 		return Integer.parseInt(hex, 16);
 	}
 	
+	// method to convert a decimal int to a hex string
 	public static String decimalToHex(int decimal) 
 	{
 		return Integer.toHexString(decimal);
 	}
 	
+	// method to convert a hex string to a binary string
 	public static String hexStringToBinaryString(String hex) 
 	{
-		long i = Long.parseLong(hex, 16);
+		Long i = Long.parseLong(hex, 16);
+
 		String binaryString = Long.toBinaryString(i);
 		
+		// add leading 0's to match address length (32-bits)
 		while (binaryString.length() != 32) 
 		{
 			binaryString = "0" + binaryString;
@@ -24,6 +35,7 @@ public class Conversions
 		return binaryString;
 	}
 	
+	// method to convert a binary string to a decimal value (literal integer)
 	public static int binaryStringToLiteralInteger(String binary) 
 	{
 		int retVal = Integer.parseInt(binary, 2);
